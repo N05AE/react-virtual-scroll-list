1034229cfdc3108e28a432da98e47a3780257b2c
Nosae <nosae@macdeiMac-Pro.local>
2022-09-15 20:19:53 +0800

    feat: init project.

--- src/components/VirtualGrid.js
+++ src/components/VirtualGrid.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import { GridWrapper, ItemContainer } from '../styles/VirtualGridStyles';
+import { virtualGridDefaultProps, virtualGridProps } from '../types';
 
 export class VirtualGrid extends Component {
-  init = a => {
-    return a;
-  };
+  static propTypes = virtualGridProps;
+  static propTypes = virtualGridDefaultProps;
 
   render() {
+    const { itemRenderer, col, row, width, height } = this.props;
     return (
-      <div>
-        Virtual Grid Virtual Grid Virtual Grid Virtual Grid Virtual Grid Virtual Grid Virtual Grid
-        Virtual Grid Virtual Grid Virtual Grid Virtual Grid Virtual Grid Virtual Grid
-      </div>
+      <GridWrapper width={width} height={height}>
+        <ItemContainer></ItemContainer>
+      </GridWrapper>
     );
   }
 }
